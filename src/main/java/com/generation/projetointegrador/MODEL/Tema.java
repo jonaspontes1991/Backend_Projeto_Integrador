7f44c32c31e0b3e37884bf856a7cc74fdcb4729f
Jonas-Pontes-Meli <jonas.pontes@mercadolivre.com>
2022-11-29 08:45:46 -0300

    mudando versão de 3.0.1  para 2.7.5

--- src/main/java/com/generation/projetointegrador/MODEL/Tema.java
+++ src/main/java/com/generation/projetointegrador/MODEL/Tema.java
@@ -1,9 +1,10 @@
 package com.generation.projetointegrador.MODEL;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "tb_temas")
