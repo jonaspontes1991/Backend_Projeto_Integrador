7de72b8375e72b5499c7c9d94f63e796e30925ad
“fbuba987” <115090534+fbuba987@users.noreply.github.com>
2022-11-30 14:33:25 -0300

    Metodo Put

--- src/main/java/com/generation/projetointegrador/CONTROLLER/TemaController.java
+++ src/main/java/com/generation/projetointegrador/CONTROLLER/TemaController.java
@@ -7,11 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.security.PublicKey;
 import java.util.List;
 
@@ -41,6 +39,13 @@ public class TemaController {
 
   }
 
+  @PutMapping
+  public ResponseEntity<Tema> put(@Valid @RequestBody Tema tema){
+    return temaRepository.findById(tema.getId())
+            .map(resposta -> ResponseEntity.status(HttpStatus.CREATED)
+                    .body(temaRepository.save(tema)))
+            .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+  }
 
 
 }
