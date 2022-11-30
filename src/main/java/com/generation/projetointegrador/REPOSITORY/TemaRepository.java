6b30e7e32ddc53833a6a1d312e2014cc40ca8ebb
Deivison Rodrigues Rocha <deivison.rocha@mercadolivre.com>
2022-11-30 11:57:59 -0300

    criando a Interface TenaRepositiry

---
+++ src/main/java/com/generation/projetointegrador/REPOSITORY/TemaRepository.java
@@ -0,0 +1,16 @@
+package com.generation.projetointegrador.REPOSITORY;
+
+import com.generation.projetointegrador.MODEL.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+
+  public List<Tema> findByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
+
+
+}
