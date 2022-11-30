8891adacb712da809ec3d1d51482e9048c70fca0
Jonas-Pontes-Meli <jonas.pontes@mercadolivre.com>
2022-11-30 14:37:44 -0300

    metodo post tema criado

--- src/main/java/com/generation/projetointegrador/CONTROLLER/TemaController.java
+++ src/main/java/com/generation/projetointegrador/CONTROLLER/TemaController.java
@@ -7,12 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.security.PublicKey;
+import javax.validation.Valid;
 import java.util.List;
 
 @Repository
@@ -40,6 +37,13 @@ public class TemaController {
       return ResponseEntity.ok(temaRepository.findByTitleContainingIgnoreCase(titulo));
 
   }
+  @PostMapping
+  public ResponseEntity postProduto(@Valid @RequestBody Tema tema)
+  {
+    return ResponseEntity.status(HttpStatus.CREATED).body(temaRepository.save(tema));
+
+  }
+
 
 
 
